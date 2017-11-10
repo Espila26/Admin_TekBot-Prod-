33e6edd25ae2a48913fab2da9ad9a9b81fc4e51d
User01 <User01@DevelopmentVM>
2017-11-10 18:20:55 +0000

    Add Case Module and fix errors on the index view.

---
+++ Admin_TekBot(Prod)/Controllers/CasesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Admin_TekBot_Prod_.Models;
+
+namespace Admin_TekBot_Prod_.Controllers
+{
+    public class CasesController : Controller
+    {
+        private BotKnowledgeDB_Entities db = new BotKnowledgeDB_Entities();
+
+        // GET: Cases
+        public ActionResult Index()
+        {
+            return View(db.Cases.ToList());
+        }
+
+        // GET: Cases/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Case @case = db.Cases.Find(id);
+            if (@case == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@case);
+        }
+
+        // GET: Cases/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cases/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CaseId,Engineer_name,Team,Case_number,Case_type,Case_Status,Case_severity,Full_Date,Day_Number,Month_Number,Week_Year,Year_Number,Day_Name,Month_Name,Comments,AssignedBy,Full_Date_Close,Create_Date,Close_Date")] Case @case)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cases.Add(@case);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(@case);
+        }
+
+        // GET: Cases/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Case @case = db.Cases.Find(id);
+            if (@case == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@case);
+        }
+
+        // POST: Cases/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CaseId,Engineer_name,Team,Case_number,Case_type,Case_Status,Case_severity,Full_Date,Day_Number,Month_Number,Week_Year,Year_Number,Day_Name,Month_Name,Comments,AssignedBy,Full_Date_Close,Create_Date,Close_Date")] Case @case)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(@case).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(@case);
+        }
+
+        // GET: Cases/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Case @case = db.Cases.Find(id);
+            if (@case == null)
+            {
+                return HttpNotFound();
+            }
+            return View(@case);
+        }
+
+        // POST: Cases/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Case @case = db.Cases.Find(id);
+            db.Cases.Remove(@case);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
